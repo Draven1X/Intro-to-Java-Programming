6812c2fb05350e0997fd8a9955e58bd0eb00251c
Draven1X <69998705+Draven1X@users.noreply.github.com>
2024-01-14 23:32:51 -0500

    Don 2-3.java

--- Exercise_02/Exercise_02_03/Exercise_02_03.java
+++ Exercise_02/Exercise_02_03/Exercise_02_03.java
@@ -1,7 +1,3 @@
-/*
-(Convert feet into meters) Write a program that reads a number in feet, converts it
-to meters, and displays the result. One foot is 0.305 meter. Here is a sample run:
-*/
 import java.util.Scanner;
 
 public class Exercise_02_03 {
